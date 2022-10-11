bba478f8f48906e16e6b303c666642b5f5883408
Michael Geyer <michael@qbitone.de>
2022-10-11 21:16:56 +0200

    chore: refactor page house

--- src/pages/house.vue
+++ src/pages/house.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import { ref, Ref, onMounted } from 'vue';
-import { useRoute } from 'vue-router';
-import { useRouter } from 'vue-router';
+import { useRouter, useRoute } from 'vue-router';
 import { useFetchData } from '../composables/useFetchData';
 import House from '../types/IHouse';
+import CaretLeft from '../components/Icons/caretLeft.vue';
+import CaretRight from '../components/Icons/caretRight.vue';
 import SpinnerComponent from '../components/spinnerComponent.vue';
 
 const route = useRoute();
@@ -11,31 +12,35 @@ const router = useRouter();
 const GoTHouse: Ref<House> = ref({}) as Ref<House>;
 const pending = ref(false);
 const error = ref(false);
+
 onMounted(async () => {
   try {
-    pending.value = true;
-    GoTHouse.value = (await useFetchData(
-      `https://anapioficeandfire.com/api/houses/${route.params.id}`
-    )) as House;
-    console.log(GoTHouse.value);
-
-    await setNameOfCurrentLord(GoTHouse.value);
-
-    pending.value = false;
+    await setGoTHouse(String(route.params.id));
   } catch (e) {
     error.value = true;
   }
 });
 
+const setGoTHouse = async (id: string) => {
+  try {
+    pending.value = true;
+    GoTHouse.value = (await useFetchData(
+      `https://anapioficeandfire.com/api/houses/${id}`
+    )) as House;
+    await setNameOfCurrentLord();
+    pending.value = false;
+  } catch (error) {}
+};
+
 /**
  * Set the name of current lords in a given array of houses.
  * @param houses
  */
-const setNameOfCurrentLord = async (house: House): Promise<void> => {
+const setNameOfCurrentLord = async (): Promise<void> => {
   try {
-    if (!house.currentLord) return; // continue if currentLord is empty
-    const { name } = await useFetchData(house.currentLord);
-    house.currentLord = name;
+    if (!GoTHouse.value.currentLord) return; // continue if currentLord is empty
+    const { name } = await useFetchData(GoTHouse.value.currentLord);
+    GoTHouse.value.currentLord = name;
   } catch (err) {
     error.value = true;
     if (import.meta.env.MODE === 'development') {
@@ -43,63 +48,103 @@ const setNameOfCurrentLord = async (house: House): Promise<void> => {
     }
   }
 };
+
+const next = async () => {
+  const nextId = Number(route.params.id) + 1;
+  router.push(`/house/${String(nextId)}`);
+  await setGoTHouse(String(nextId));
+};
+
+const previous = async () => {
+  const nextId = Number(route.params.id) - 1;
+  router.push(`/house/${String(nextId)}`);
+  await setGoTHouse(String(nextId));
+};
 </script>
 <template>
-  <SpinnerComponent v-if="pending" />
-  <button @click="router.push('/')">back</button>
-  <div
-    v-if="!pending"
-    class="overflow-hidden bg-white shadow sm:rounded-lg max-w-3xl mx-auto"
-  >
-    <div class="px-4 py-5 sm:px-6">
-      <h3 class="text-lg font-medium leading-6 text-gray-900">
-        {{ GoTHouse.name }}
-      </h3>
-      <p class="mt-1 max-w-2xl text-sm text-gray-500">
-        Detailed house information
-      </p>
+  <section class="py-24 mx-auto">
+    <div class="flex justify-between py-2 max-w-3xl mx-auto">
+      <button
+        @click="router.push('/')"
+        class="py-2 px-4 border-2 rounded-lg hover:bg-slate-100 group"
+      >
+        <span class="text-gray-500 group-hover:text-black"> Back </span>
+      </button>
+      <div class="flex gap-2">
+        <button
+          v-if="route.params.id != '1'"
+          @click="previous"
+          class="py-2 px-4 border-2 rounded-lg group hover:bg-slate-100"
+        >
+          <CaretLeft class="h-6 w-6 text-gray-500 group-hover:text-black" />
+        </button>
+        <button
+          @click="next"
+          class="py-2 px-4 border-2 rounded-lg group hover:bg-slate-100"
+        >
+          <CaretRight class="h-6 w-6 text-gray-500 group-hover:text-black" />
+        </button>
+      </div>
     </div>
-    <div class="border-t border-gray-200">
-      <dl>
-        <div
-          class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
-        >
-          <dt class="text-sm font-medium text-gray-500">Lord</dt>
-          <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-            {{ GoTHouse.currentLord || '-' }}
-          </dd>
-        </div>
-        <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-          <dt class="text-sm font-medium text-gray-500">Region</dt>
-          <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-            {{ GoTHouse.region || '-' }}
-          </dd>
-        </div>
-        <div
-          class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
-        >
-          <dt class="text-sm font-medium text-gray-500">Coat of Arms</dt>
-          <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-            {{ GoTHouse.coatOfArms || '-' }}
-          </dd>
-        </div>
-        <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-          <dt class="text-sm font-medium text-gray-500">Words</dt>
-          <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-            {{ GoTHouse.words || '-' }}
-          </dd>
-        </div>
-        <div
-          class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
-        >
-          <dt class="text-sm font-medium text-gray-500">Titles</dt>
-          <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-            <ul>
-              <li v-for="value in GoTHouse.titles">{{ value || '-' }}</li>
-            </ul>
-          </dd>
-        </div>
-      </dl>
+    <SpinnerComponent v-if="pending" class="block mx-auto" />
+    <div
+      v-if="!pending"
+      class="max-w-3xl mx-auto rounded-lg border-2 border-gray-200 border-opacity-50"
+    >
+      <div class="px-4 py-5 sm:px-6">
+        <h3 class="text-lg font-extrabold text-gray-900">
+          {{ GoTHouse.name }}
+        </h3>
+        <p class="mt-1 max-w-2xl text-sm text-gray-500">
+          Detailed house information
+        </p>
+      </div>
+      <div class="border-t border-gray-200">
+        <dl>
+          <div
+            class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
+          >
+            <dt class="text-sm font-bold">Lord</dt>
+            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+              {{ GoTHouse.currentLord || '-' }}
+            </dd>
+          </div>
+          <div
+            class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
+          >
+            <dt class="text-sm font-bold">Region</dt>
+            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+              {{ GoTHouse.region || '-' }}
+            </dd>
+          </div>
+          <div
+            class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
+          >
+            <dt class="text-sm font-bold">Coat of Arms</dt>
+            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+              {{ GoTHouse.coatOfArms || '-' }}
+            </dd>
+          </div>
+          <div
+            class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
+          >
+            <dt class="text-sm font-bold">Words</dt>
+            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+              {{ GoTHouse.words || '-' }}
+            </dd>
+          </div>
+          <div
+            class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
+          >
+            <dt class="text-sm font-bold">Titles</dt>
+            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+              <ul>
+                <li v-for="value in GoTHouse.titles">{{ value || '-' }}</li>
+              </ul>
+            </dd>
+          </div>
+        </dl>
+      </div>
     </div>
-  </div>
+  </section>
 </template>
