5ab855e2fb7adfc0e8fa1a75bd7331508441f6bc
Michael Geyer <michael@qbitone.de>
2022-10-11 12:48:21 +0200

    chore: update useGetHouses and set fetch 24 items by default

--- src/composables/useGetHouses.ts
+++ src/composables/useGetHouses.ts
@@ -1,10 +1,16 @@
 import { useFetchData } from './useFetchData';
 import House from '../types/IHouse';
 
+/**
+ * Get an array of 24 GoT Houses by a given page
+ *
+ * @param page
+ * @returns Array of Houses
+ */
 export const useGetHouses = async (page: number = 1): Promise<House[]> => {
   try {
     const houses = (await useFetchData(
-      'https://anapioficeandfire.com/api/houses?page=1&pageSize=10'
+      `https://anapioficeandfire.com/api/houses?page=${page}&pageSize=24`
     )) as House[];
     return houses;
   } catch (error) {
