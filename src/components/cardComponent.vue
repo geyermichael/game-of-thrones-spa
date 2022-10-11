86d22a832162580fdcbf6de994c6bb7b7b095001
Michael Geyer <michael@qbitone.de>
2022-10-11 21:15:56 +0200

    chore: minor ui improvment

--- src/components/cardComponent.vue
+++ src/components/cardComponent.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import CaretRight from './Icons/caretRight.vue';
 const props = defineProps({
   name: {
     type: String,
@@ -19,9 +20,9 @@ const props = defineProps({
 });
 </script>
 <template>
-  <div class="p-4 lg:w-1/3 md:w-full cursor-pointer">
+  <div class="p-4 lg:w-1/3 md:w-1/2 cursor-pointer">
     <div
-      class="flex border-2 rounded-lg border-gray-200 border-opacity-50 p-8 sm:flex-row flex-col hover:bg-slate-100"
+      class="flex border-2 rounded-lg border-gray-200 p-8 sm:flex-row flex-col hover:bg-slate-100"
     >
       <div class="flex-grow">
         <h2 class="text-lg font-extrabold mb-3">
@@ -43,22 +44,12 @@ const props = defineProps({
         </dl>
 
         <a
-          class="inline-flex justify-center mt-3 text-red-800 items-center cursor-pointer group"
+          class="inline-flex justify-center items-center mt-3 text-red-800 cursor-pointer group"
         >
           <span class="group-hover:mr-1 transition-all ease-out"
             >More About</span
           >
-          <svg
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            class="w-4 h-4 ml-2"
-            viewBox="0 0 24 24"
-          >
-            <path d="M5 12h14M12 5l7 7-7 7"></path>
-          </svg>
+          <CaretRight class="w-5 h-5" />
         </a>
       </div>
     </div>
