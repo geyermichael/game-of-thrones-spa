c4d81277491fd526afafb7ec14ab1516b25402cd
Michael Geyer <53266677+geyermichael@users.noreply.github.com>
2026-01-01 17:44:52 +0100

    Fix header text in TheHeader component

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <header>
     <div class="container mx-auto text-center p-12">
-      <h1 class="text-3xl font-got capitalize">Game of Thrones Houses - should works</h1>
+      <h1 class="text-3xl font-got capitalize">Game of Thrones Houses</h1>
     </div>
   </header>
 </template>
